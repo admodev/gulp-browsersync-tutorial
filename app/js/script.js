ed433974a45e5b89c46d9c6ab3af09df500cc348
admodev <adolfomoyano@exegesisba.com>
2021-08-16 22:20:31 -0300

    finish tutorial

---
+++ app/js/script.js
@@ -0,0 +1,11 @@
+/** @format */
+
+const testString = 'This is a test, reloading!!';
+
+console.log(testString);
+
+const fruits = ['mango', 'papaya', 'lychee'];
+
+fruits.map((fruit) => {
+    console.log(fruit);
+});
---
+++ dist/script.js
@@ -0,0 +1,2 @@
+const testString="This is a test, reloading!!";console.log(testString);const fruits=["mango","papaya","lychee"];fruits.map((t=>{console.log(t)}));
+//# sourceMappingURL=script.js.map
